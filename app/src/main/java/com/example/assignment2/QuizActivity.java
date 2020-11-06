3420612143e315908721345a225f6aefd5c2dd67
kevino29 <varax297@gmail.com>
2020-11-06 18:09:39 -0400

    got the quiz algorithm working

--- app/src/main/java/com/example/assignment2/QuizActivity.java
+++ app/src/main/java/com/example/assignment2/QuizActivity.java
@@ -2,26 +2,27 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.*;
 import android.os.*;
 import android.util.*;
-import android.view.View;
+import android.view.*;
 import android.widget.*;
-
 import java.io.*;
 import java.util.*;
 
 public class QuizActivity extends AppCompatActivity {
-    private ArrayList<Button> options = new ArrayList<>();
-    private ArrayList<String> questions = new ArrayList<>();
-    private ArrayList<String> answers = new ArrayList<>();
+    private HashMap<String, String> quiz;
+    private ArrayList<Button> options;
+    private ArrayList<String> answerOptions;
+    private ArrayList<String> answers;
     private String answer;
+    private String correctAnswer;
     private TextView tvQuestionNum;
     private TextView tvQuestion;
     private ProgressBar pbProgress;
     private int progress;
-    private InputStream is;
-    private BufferedReader br;
+    private int questionNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +51,24 @@ public class QuizActivity extends AppCompatActivity {
             }
             options.get(i).setOnClickListener(new OptionButtonClicked());
         }
-        showQuiz();
+        startQuiz();
     }
 
     private void init() {
+        quiz = new HashMap<>();
+        options = new ArrayList<>();
+        answerOptions = new ArrayList<>();
+        answers = new ArrayList<>();
         answer = null;
+        correctAnswer = null;
         progress = 0;
+        questionNumber = 0;
         getQuizData();
     }
 
     private void getQuizData() {
+        InputStream is;
+        BufferedReader br;
         String input = "";
         String[] inputs = new String[2];
 
@@ -69,8 +78,8 @@ public class QuizActivity extends AppCompatActivity {
 
             while ((input = br.readLine()) != null) {
                 inputs = input.split("@");
-                questions.add(inputs[0]);
                 answers.add(inputs[1]);
+                quiz.put(inputs[1], inputs[0]);
             }
         }
         catch (IOException e) {
@@ -85,28 +94,78 @@ public class QuizActivity extends AppCompatActivity {
         }
     }
 
-    private void showQuiz() {
+    private void startQuiz() {
+        Collections.shuffle(answers);
+        questionNumber = 0;
 
+        showQuiz(questionNumber);
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void showQuiz(int questionNum) {
+        correctAnswer = answers.get(questionNum);
+        if (answerOptions.size() > 0)
+            answerOptions.clear();
+
+        tvQuestionNum.setText(String.format("Question %d", questionNum+1));
+        tvQuestion.setText(quiz.get(correctAnswer));
+        quiz.remove(correctAnswer);
+
+        answerOptions.add(correctAnswer);
+        while (answerOptions.size() != 4) {
+            String temp = answers.get(getRandomNum(0, 9));
+
+            if (!answerOptions.contains(temp))
+                answerOptions.add(temp);
+        }
+        Collections.shuffle(answerOptions);
+
+        int c = 0;
+        for (Button button : options) {
+            button.setText(answerOptions.get(c++));
+        }
+    }
+
+    private void finishQuiz() {
+        TableLayout tlOptions = findViewById(R.id.tlOptions);
+        TableLayout tlContinue = findViewById(R.id.tlContinue);
+        TableRow trQuestion = findViewById(R.id.row1);
+        Button btnContinue = findViewById(R.id.btnContinue);
+
+        tlOptions.setVisibility(View.GONE);
+        trQuestion.setVisibility(View.GONE);
+        tlContinue.setVisibility(View.VISIBLE);
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle extras = getIntent().getExtras();
+                Intent intent = new Intent("");
+                startActivity(intent);
+            }
+        });
+    }
+
+    private int getRandomNum(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
     }
 
     private class OptionButtonClicked implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            progress++;
-            switch (view.getId()) {
-                case R.id.btnOption1:
-                    String s = null;
-                    break;
-                case R.id.btnOption2:
-                    break;
-                case R.id.btnOption3:
-                    String d = "something";
-                    break;
-                case R.id.btnOption4:
-                    String a = "nothing";
-                    break;
-            }
-            pbProgress.setProgress(progress);
+            String inputAnswer = ((Button)view).getText().toString();
+
+            if (inputAnswer.equals(correctAnswer))
+                Toast.makeText(getApplicationContext(), "That is the correct answer!", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(getApplicationContext(), "That is the wrong answer!", Toast.LENGTH_SHORT).show();
+
+            pbProgress.setProgress(++progress);
+
+            if (quiz.size() != 0)
+                showQuiz(++questionNumber);
+            else
+                finishQuiz();
         }
     }
 }
